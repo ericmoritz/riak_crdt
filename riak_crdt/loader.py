9154bce6af45e3450176acb2e52417b3cccd5e26
Eric Moritz <eric@themoritzfamily.com>
2011-10-06 11:20:31 -0400

    * Added the context manager and the ability to abort a batch operation * Fixed the merge for 1.0 which may receive tombstone siblings with empty   values. * Renamed the reload method to refresh to match the RiakObject method name * Removed the reset method.  If devs want to reset a CRDT, we'll let them   do it manually.  I don't think this is a use case that needs   a dedicated method (I may be wrong).

--- riak_crdt/loader.py
+++ riak_crdt/loader.py
@@ -1,75 +1,118 @@
 
+
+class Abort(Exception):
+    pass
+
+
 class Loader(object):
     def __init__(self, crdt_class, bucket, key,
                  content_type="application/json"):
         self.bucket = bucket
         self.key = key
         self.crdt_class = crdt_class
+        self.content_type = content_type
+
+        # Internal caches
         self._crdt_obj = None
         self._riak_obj = None
-        self.content_type = content_type
 
     @property
     def obj(self):
         if self._crdt_obj is None:
             self.load()
 
-        # Return the cached crdt object
         return self._crdt_obj
 
     def load(self):
         riak_obj = self.bucket.get(self.key)
 
         if riak_obj.has_siblings():
-            crdt_obj = self.merge(riak_obj)
-        elif riak_obj.exists():
-            payload = riak_obj.get_data()
-            crdt_obj = self.crdt_class.from_payload(payload)
+            riak_obj, crdt_obj = self.merge(riak_obj)
         else:
-            crdt_obj = self.crdt_class()
+            if riak_obj.exists():
+                payload = riak_obj.get_data()
+            else:
+                payload = ""
+
+            if payload:
+                crdt_obj = self.crdt_class.from_payload(payload)
+            else:
+                crdt_obj = self.crdt_class()
 
         # Cache the riak object and crdt objects
         self._riak_obj = riak_obj
         self._crdt_obj = crdt_obj
-        
-    def reset(self):
-        self._crdt_obj = self.crdt_class()
 
-    def reload(self):
+    def refresh(self):
         self.load()
         return self._crdt_obj
-            
+
     def merge(self, riak_obj):
         siblings = riak_obj.get_siblings()
+        #
+        # In Riak 1.0, tombstone siblings comeback with no data so we need
+        # to filter those out.  This occurs on concurrent DELETE and PUTs
+        # I am not sure
+        siblings = [obj for obj in siblings
+                    if obj.get_data() != ""]
+
         first = siblings[0]
         others = siblings[1:]
 
-        sibling_payloads = (obj.get_data() for obj in others)
-        sibling_crdts = (self.crdt_class.from_payload(p) \
-                             for p in sibling_payloads)
-
+        # Create the starting point using the first sibling
         crdt = self.crdt_class.from_payload(first.get_data())
-        for other in sibling_crdts:
-            crdt = self.crdt_class.merge(crdt, other)
 
-        return crdt
+        # Merge the other siblings to the first one
+        for other in others:
+            other_crdt = self.crdt_class.from_payload(other.get_data())
+            crdt = self.crdt_class.merge(crdt, other_crdt)
+
+        # Return one of the siblings because they have the VClock value
+        return first, crdt
 
     def commit(self):
+        """Stores the CRDT into Riak"""
         # Can't commit unloaded data
         if self._crdt_obj is not None:
-            payload = self._crdt_obj.payload
-
-            self._riak_obj.set_data(payload)
+            self._riak_obj.set_data(self._crdt_obj.payload)
+            #
+            # Sometimes the content_type isn't set for a source object
+            # for instance on 404s
             self._riak_obj.set_content_type(self.content_type)
+            #
             self._riak_obj.store()
-
-            # Clear the cache
+            #
+            # Clear the cache so that the next access to the obj
+            # property will result in a reload
             self._riak_obj = None
             self._crdt_obj = None
+
+    @classmethod
+    def abort(cls):
+        """Aborts a with block batch operation:
+
+        with Loader(LWWSet, bucket, "foo") as obj:
+            # If "eric" is not in the set, abort the batch
+            if "eric" not in obj:
+                Loader.abort()
+            else:
+                # otherwise, add my parents
+                obj.add("eric")
+        """
+        raise Abort()
 
     def __enter__(self):
         return self.obj
 
     def __exit__(self, exc_type, exc_value, traceback):
+        # Swallow up aborts without committing
+        if exc_type is Abort:
+            return True
+
+        # If no exception happened within the with block, commit the
+        # object
         if exc_type is None:
             self.commit()
+
+        # Otherwise, return False which tells Python to reraise any exceptions
+        return False
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -3,53 +3,77 @@ import unittest
 import riak
 import os
 from crdt.sets import LWWSet
-from riak_crdt.loader import Loader
+from riak_crdt import Loader
+import logging
 
-client = riak.RiakClient(transport_class=riak.RiakPbcTransport,
-                         port=8081)
+logging.basicConfig(level=logging.DEBUG)
+
+client = riak.RiakClient(port=8091)
+
 
 class TestLoader(unittest.TestCase):
     def setUp(self):
-        super(TestLoader, self).tearDown()
-
-    def tearDown(self):
-        bucket = client.bucket("test")
-        obj = bucket.get("test")
+        self.bucket = client.bucket("test-crdt")
+        obj = self.bucket.get("test")
         obj.delete()
-        
+
     def test_siblings(self):
-        bucket = client.bucket("test")
-        bucket.set_allow_multiples(True)
+        self.bucket.set_allow_multiples(True)
 
         # Create the source object
-        loader = Loader(LWWSet, bucket, "test")
+        loader = Loader(LWWSet, self.bucket, "test")
         loader.obj.add("eric")
         loader.commit()
 
         # Create two siblings
-        loader1 = Loader(LWWSet, bucket, "test")
+        loader1 = Loader(LWWSet, self.bucket, "test")
         loader1.obj.add("glenn")
 
-        loader2 = Loader(LWWSet, bucket, "test")
+        loader2 = Loader(LWWSet, self.bucket, "test")
         loader2.obj.add("mark")
-
 
         loader1.commit()
         loader2.commit()
 
-        loader3 = Loader(LWWSet, bucket, "test")
+        loader3 = Loader(LWWSet, self.bucket, "test")
+        loader3.load()
+        loader3.commit()
 
-        self.assertEqual(loader3.obj.value, 
+        self.assertEqual(loader3.obj.value,
                          set(["glenn", "mark", "eric"]))
 
     def test_contextmanager(self):
-        bucket = client.bucket("test")
-
-        with Loader(LWWSet, bucket, "test") as obj:
+        with Loader(LWWSet, self.bucket, "test") as obj:
             obj.add("eric")
             obj.add("glenn")
             obj.add("foo")
-            
-        loader = Loader(LWWSet, bucket, "test")
+
+        loader = Loader(LWWSet, self.bucket, "test")
         self.assertEqual(loader.obj.value,
                          set(["eric", "glenn", "foo"]))
+
+    def test_refresh(self):
+        loader = Loader(LWWSet, self.bucket, "test")
+        # sanity check
+        self.assertTrue("eric" not in loader.obj)
+        loader.obj.add("eric")
+
+        self.assertTrue("eric" in loader.obj)
+
+        loader.refresh()
+        self.assertTrue("eric" not in loader.obj)
+
+    def test_abort(self):
+        loader = Loader(LWWSet, self.bucket, "test")
+        # sanity check
+        self.assertTrue("eric" not in loader.obj)
+
+        with Loader(LWWSet, self.bucket, "test") as obj:
+            obj.add("eric")
+            Loader.abort()
+
+        # Fetch the object again
+        loader.refresh()
+
+        # sanity check
+        self.assertTrue("eric" not in loader.obj)
